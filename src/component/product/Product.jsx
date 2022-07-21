eb13808ec5406ced5f0249eee2dabefb59ddb1ae
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-07-21 23:33:59 +0300

    google firestore added to project

--- src/component/product/Product.jsx
+++ src/component/product/Product.jsx
@@ -1,32 +1,49 @@
+import './product.css'
+import { Link } from 'react-router-dom'
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Product = () => {
+
+  const { currentUser } = useContext(AuthContext)
+
   return (
-    <div>
+    <div className="product">
       <section>
-      <div className="heroContent">
-                <h2 className="heroTitle" >
-                    Worlds largest quiz web site  And yes! it is free
-                </h2>
-                <p className="heroDesc">
-                    If you bored come and try some of our questions
-                </p>
-            </div>
-            <div className="heroContent">
-                <h2 className="heroTitle" >
-                    Worlds largest quiz web site  And yes! it is free
-                </h2>
-                <p className="heroDesc">
-                    If you bored come and try some of our questions
-                </p>
-            </div>
-            <div className="heroContent">
-                <h2 className="heroTitle" >
-                    Worlds largest quiz web site  And yes! it is free
-                </h2>
-                <p className="heroDesc">
-                    If you bored come and try some of our questions
-                </p>
-            </div>
+        <div className="productContent">
+          <Link to="/quiz" >
+            <h2 className="productTitle" >
+              QQ
+            </h2>
+            <p className="productDesc">
+              QQ means is "Quick Quiz" you can select category, difficulty, number of question and you are ready to practice
+            </p>
+          </Link>
+        </div>
+      </section>
+      <section>
+        <div className="productContent">
+        <Link to="/" >
+          <h2 className="productTitle" >
+            Take an exam
+          </h2>
+          <p className="productDesc">
+            after you logged in you can press button and after write your pass you can make ur online exam
+          </p>
+          </Link>
+        </div>
+      </section>
+      <section>
+        <div className="productContent">
+        <Link to="/" >
+          <h2 className="productTitle" >
+            Make an exam
+          </h2>
+          <p className="productDesc">
+            come upload your questions then share your pass and make your exam
+          </p>
+          </Link>
+        </div>
       </section>
     </div>
   )
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,42 +1,61 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './register.css'
-import { auth } from "../../firebase-config"
+import { auth, db } from "../../firebase-config"
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc , serverTimestamp } from "firebase/firestore";
 
 
 const Register = () => {
 
-  const [email, setEmail]=useState("");
-  const [password, setPassword]=useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");//google firebase otomatik bir şekilde şifrelediği için bir şifreleme kullanmadım passworda
+  const [name, setName] = useState("");
+  const [lName, setLName] = useState("");
+  const [username, setUsername] = useState("");
+  const [cPassword, setCPassword] = useState("");
 
-  const handleLogin = (e) => {
+
+
+  const handleRegister = async (e) => {
+  
     e.preventDefault();
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
+      await setDoc (doc(db, "users",res.user.uid), {
+        first: name,
+        last: lName,
+        username: username,
+        email: email,
+        timeStamp: serverTimestamp(),
       });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    
   }
 
   return (
     <div className='register'>
       <div className='wrapperR'>
         <h1>Create An Account</h1>
-        <form className='formRegister' onSubmit={handleLogin}>
-          <input className='inputFR1' type="text" placeholder='name' required />
-          <input className='inputFR1' type="text" placeholder='last name' required />
-          <input className='inputFR1' type="text" placeholder='username' required />
-          <input className='inputFR1' type="email" placeholder='email' onChange={e=>setEmail(e.target.value)} required />
-          <input className='inputFR1' type="password" placeholder='password' onChange={e=>setPassword(e.target.value)}  required/>
-          <input className='inputFR1' type="password" placeholder='confirm password' required/>
-          <button className='formRButton' type='submit'>Create</button>
+        <form className='formRegister' onSubmit={handleRegister}>
+          <input className='inputFR1' type="text" placeholder='name' onChange={e => setName(e.target.value)} required/>
+          <input className='inputFR1' type="text" placeholder='last name' onChange={e => setLName(e.target.value)} required />
+          <input className='inputFR1' type="text" placeholder='username' onChange={e => setUsername(e.target.value)} required />
+          <input className='inputFR1' type="email" placeholder='email' onChange={e => setEmail(e.target.value)} required />
+          <input className='inputFR1' type="password" placeholder='password' onChange={e => setPassword(e.target.value)} required />
+          <input className='inputFR1' type="password" placeholder='confirm password' onChange={e => setCPassword(e.target.value)} required />
+          {password==cPassword ?(
+            <>
+             <button className='formRButton' type='submit'>Create</button>
+            </>
+          ):(
+            <>
+             <button className='formRButton' disabled={true} style={{cursor:"default"}}>Check your password</button>
+            </>
+          )}
           <p>Do you have an account? <Link to="/login" className='rLink'>Login</Link></p>
         </form>
       </div>
